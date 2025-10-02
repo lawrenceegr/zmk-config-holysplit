c270dd94554c21f91d8187f3d73c29f654bad0bb
lawrenceegr <lawrencelangatmi@gmail.com>
2025-10-02 11:48:25 +0300

    initial re-work

---
+++ config/boards/arm/holyiot_18010/board.cmake
@@ -0,0 +1,7 @@
+# Copyright (c) 2024
+# SPDX-License-Identifier: MIT
+
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
